5726ba26b292a1630b255e80f168b907c05ef1a0
Critzblank <jc675@njit.edu>
2016-09-29 13:35:13 -0400

    added comments

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
-
+	//this is how you print text in php
 	echo 'hello world';
+	//this is how you store a value in a variable
+	$myvar = 'some Text I want to store in a variable';
+	
+	//this is an example between single quotes and double quotes
+	echo '<br>';
+	echo '$myvar';
+	echo '<br>';
+	echo "$myvar";
+
 
 ?>
 
